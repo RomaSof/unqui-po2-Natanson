bea366e535c63af1b781a22ab842e63b5ea2357d
RomaSof <sofiarnatanson@gmail.com>
2025-04-17 04:02:52 -0300

    arreglo carpeta tests

--- tests/unq/MultiplicationTableTestCase.java
+++ tests/unq/MultiplicationTableTestCase.java
@@ -1,10 +1,12 @@
-package ar.edu.unq.po2.tp3;
+package unq;
 
 import static org.junit.jupiter.api.Assertions.*;
 //import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.tp3.MultiplicationTable;
 
 class MultiplicationTableTestCase {
 	private MultiplicationTable mult; 
